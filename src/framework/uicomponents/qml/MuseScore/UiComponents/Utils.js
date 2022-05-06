fd03c16e133fc635fddf4a4950bd99b40bb032c5
Josh Soref <jsoref@users.noreply.github.com>
2022-05-06 17:31:21 -0400

    spelling: length
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/framework/uicomponents/qml/MuseScore/UiComponents/Utils.js
+++ src/framework/uicomponents/qml/MuseScore/UiComponents/Utils.js
@@ -69,7 +69,7 @@ function getItem(model, row) {
 function makeMnemonicText(text) {
     var index = text.indexOf("&")
     if (index !== -1) {
-        if (index === text.lenght - 1) {
+        if (index === text.length - 1) {
             text = removeAmpersands(text)
         } else {
             text = text.replace(text.substr(index, 2), ("<u>" + text.substr(index + 1, 1) + "</u>"))
